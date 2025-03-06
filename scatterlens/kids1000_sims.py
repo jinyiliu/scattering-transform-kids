4f85bf6e1b27b2019a6b4e4dd2a0b6a1d7860c4b
Jinyi Liu <liujy0129@gmail.com>
2025-03-06 23:31:49 +0100

    Move all KiDS-1000 related properties and functions into a class

--- scatterlens/kids1000_sims.py
+++ scatterlens/kids1000_sims.py
@@ -5,39 +5,59 @@ from numpy.typing import NDArray
 
 _data_path = "/data1/jliu/scattering-transform-kids/data/KiDS-1000/MassMaps4JHD/"
 
-_zbin_to_ShapeNoise = {
-    1: 0.270,
-    2: 0.258,
-    3: 0.273,
-    4: 0.254,
-    5: 0.270,
-}
-
-_zbin_to_zrange = {
-    1: "0.1-0.3",
-    2: "0.3-0.5",
-    3: "0.5-0.7",
-    4: "0.7-0.9",
-    5: "0.9-1.2",
-}
-
-def _has_cosmol(cosmol: int) -> bool:
-    # In SLICE simulation set has 25 wCDM cosmologies and one Lambda-CDM cosmology.
-    # -1 points to fiducial cosmology
-    return -1 <= cosmol <= 24
-
-def _has_region(region: int) -> bool:
-    return 1 <= region <= 18
-
-def _has_zbin(zbin: int) -> bool:
-    return isinstance(zbin, int) and 1 <= zbin <= 5
-
-class CosmoSLICE:
+class KiDS1000:
     resol = 2.344
     resol_unit = "arcmin"
+    zbin_to_ShapeNoise = {
+        1: 0.270,
+        2: 0.258,
+        3: 0.273,
+        4: 0.254,
+        5: 0.270,
+    }
+    zbin_to_zrange = {
+        1: "0.1-0.3",
+        2: "0.3-0.5",
+        3: "0.5-0.7",
+        4: "0.7-0.9",
+        5: "0.9-1.2",
+    }
+    region_MN = {
+        1: (127, 115),
+        2: (127, 221),
+        3: (152, 255),
+        4: (152, 255),
+        5: (152, 255),
+        6: (152, 255),
+        7: (152, 255),
+        8: (152, 255),
+        9: (177, 255),
+        10: (103, 255),
+        11: (224, 253),
+        12: (205, 252),
+        13: (205, 251),
+        14: (205, 250),
+        15: (180, 250),
+        16: (180, 250),
+        17: (223, 253),
+        18: (224, 159),
+    }
+
+    @staticmethod
+    def has_region(region: int) -> bool:
+        return 1 <= region <= 18
+
+    @staticmethod
+    def has_zbin(zbin: int) -> bool:
+        return isinstance(zbin, int) and 1 <= zbin <= 5
+
+
+class CosmoSLICE(KiDS1000):
     simspath = "MRres140.64arcs_100Sqdeg_SN{:g}_Mosaic_KiDS1000GpAM_zKiDS1000_{:s}_Cosmol{:d}"
     simspath_fid = "MRres140.64arcs_100Sqdeg_SN{:g}_Mosaic_KiDS1000GpAM_zKiDS1000_{:s}_Cosmolfid"
     mapfname = r"SN{:g}_Mosaic.KiDS1000GpAM.LOS{}R{:d}.SS2.816.Ekappa.npy"
+    LOS_indices = list(range(1, 51))
+    simsname = "KiDS1000_CosmoSLICE"
 
     @staticmethod
     def get_sim_massmap(
@@ -48,16 +68,16 @@ class CosmoSLICE:
             region: int,
     ) -> NDArray:
         assert (
-            _has_cosmol(cosmol)
-            and _has_region(region)
-            and _has_zbin(zbin1)
-            and _has_zbin(zbin2)
-            and CosmoSLICE._has_LOS(LOS)
+            CosmoSLICE.has_cosmol(cosmol)
+            and KiDS1000.has_region(region)
+            and KiDS1000.has_zbin(zbin1)
+            and KiDS1000.has_zbin(zbin2)
+            and CosmoSLICE.has_LOS(LOS)
         ), "Validation failed for one or more inputs."
 
-        shapenoise = _zbin_to_ShapeNoise[zbin1]
-        zbcut = f"ZBcut{_zbin_to_zrange[zbin1]}"
-        zbcut += f"_X_ZBcut{_zbin_to_zrange[zbin2]}" if zbin2 != zbin1 else ""
+        shapenoise = KiDS1000.zbin_to_ShapeNoise[zbin1]
+        zbcut = f"ZBcut{KiDS1000.zbin_to_zrange[zbin1]}"
+        zbcut += f"_X_ZBcut{KiDS1000.zbin_to_zrange[zbin2]}" if zbin2 != zbin1 else ""
 
         if cosmol==-1:
             simspath = CosmoSLICE.simspath_fid.format(shapenoise, zbcut)
@@ -84,22 +104,30 @@ class CosmoSLICE:
         return massmap
 
     @staticmethod
-    def _has_LOS(LOS: int) -> bool:
+    def has_LOS(LOS: int) -> bool:
         return 1 <= LOS <= 50
 
-
-
-class SLICE:
     @staticmethod
-    def get_sim_kappa(self, region):
+    def has_cosmol(cosmol: int) -> bool:
+        # SLICE simulation set has 25 wCDM cosmologies and one Lambda-CDM cosmology.
+        # -1 points to fiducial cosmology
+        return -1 <= cosmol <= 24
+
+
+
+class SLICE(KiDS1000):
+    simsname = "KiDS1000_SLICE"
+
+    @staticmethod
+    def get_sim_massmap(region):
         pass
 
     @staticmethod
-    def _has_LOS(LOS: int) -> bool:
+    def has_LOS(LOS: int) -> bool:
         pass
 
 
-def _read_cosmologies_info():
+def _read_cosmologies_info() -> pd.DataFrame:
     fname = os.path.join(os.path.dirname(__file__), "data", "kids1000_cosmol.csv")
     df = pd.read_csv(
         fname, delimiter='\t', skiprows=1,
