914ddb9abc1d8d2eb28dcfcdd8ab09d384fe3145
Jinyi Liu <liujy0129@gmail.com>
2026-05-13 13:23:58 +0200

    Include baryonic feedback in the model

--- scatterlens/model.py
+++ scatterlens/model.py
@@ -9,7 +9,8 @@ class Model:
             self,
             emulator: Emulator,
             systematics: list[str] | None=None,
-            IA_kwargs: dict | None = None,
+            IA_kwargs: dict | None=None,
+            baryon_kwargs: dict | None=None,
     ):
         """Forward model the statistics with systematic effects.
 
@@ -19,14 +20,25 @@ class Model:
         self.emulator = emulator
 
         if systematics:
-            if not any(syst in ("IA",) for syst in systematics):
+            if not any(syst in ("IA", "baryon") for syst in systematics):
                 raise ValueError
         self.systematics = systematics
 
         if IA_kwargs:
+            assert "savepath" in IA_kwargs
             self.IA_kwargs = IA_kwargs
-            self.IA_poly_coefs = np.array(self._load_IA_poly_coefs())
+            self.IA_poly_coefs = np.array(
+                torch.load(IA_kwargs["savepath"], weights_only=True)
+            )
             self.IA_fid = self._get_IA_fid()
+
+        if baryon_kwargs:
+            assert "savepath" in baryon_kwargs
+            self.baryon_kwargs = baryon_kwargs
+            self.baryon_poly_coefs = np.array(
+                torch.load(baryon_kwargs["savepath"], weights_only=True)
+            )
+            self.baryon_fid = self._get_baryon_fid()
 
 
     def predict(self, params) -> np.ndarray:
@@ -36,6 +48,8 @@ class Model:
                 match syst:
                     case "IA":
                         dv *= self.IA(param)
+                    case "baryon":
+                        dv *= self.baryon(param)
                     case _:
                         pass
         return dv
@@ -53,6 +67,16 @@ class Model:
         IA_scaling = (X @ self.IA_poly_coefs).squeeze(0) / self.IA_fid
         return IA_scaling
 
+    def baryon(self, b_bary) -> np.ndarray:
+        """Baryonic feedback model.
+
+        Args:
+            b_bary: Baryonic feedback parameter.
+        """
+        degree = self.baryon_poly_coefs.shape[0] - 1
+        X = np.vander(x=[b_bary], N=degree + 1, increasing=False)
+        baryon_scaling = (X @ self.baryon_poly_coefs).squeeze(0) / self.baryon_fid
+        return baryon_scaling
 
     def _get_IA_fid(self):
         A_IA = 0.
@@ -61,10 +85,18 @@ class Model:
         IA_fid = (X @ self.IA_poly_coefs).squeeze(0)
         return IA_fid
 
+    def _get_baryon_fid(self):
+        b_bary = 0.
+        degree = self.baryon_poly_coefs.shape[0] - 1
+        X = np.vander(x=[b_bary], N=degree + 1, increasing=False)
+        baryon_fid = (X @ self.baryon_poly_coefs).squeeze(0)
+        return baryon_fid
 
-    def _load_IA_poly_coefs(self) -> torch.Tensor:
-        assert "savepath" in self.IA_kwargs
-        return torch.load(self.IA_kwargs["savepath"], weights_only=True)
+
+    @staticmethod
+    def _load_poly_coefs(kwargs) -> torch.Tensor:
+        assert "savepath" in kwargs
+        return torch.load(kwargs["savepath"], weights_only=True)
 
 
     @staticmethod
